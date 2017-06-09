bec3eb5158bbc6e8f4bc45c6b78ce09b0193118e
egrosner <egrosner@gmail.com>
2017-06-08 23:12:45 -0400

    fixed message contents

--- src/main/java/com/erich/grosner/slackmoviemaster/config/ResponseConfig.java
+++ src/main/java/com/erich/grosner/slackmoviemaster/config/ResponseConfig.java
@@ -4,21 +4,15 @@ import com.erich.grosner.slackmoviemaster.model.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.google.common.io.Resources;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by egros on 6/8/2017.
@@ -28,10 +22,10 @@ public class ResponseConfig {
     @Bean
     public List<Response> getResponses() throws URISyntaxException, IOException {
         URI messages = Resources.getResource("terminator.json").toURI();
-        File f = Paths.get(messages).toFile();
+        String contents = new String(Files.readAllBytes(Paths.get(messages)));
 
         ObjectMapper om = new ObjectMapper();
-        List<Response> responses = om.readValue(f, TypeFactory.defaultInstance().constructCollectionType(List.class, Response.class));
+        List<Response> responses = om.readValue(contents, TypeFactory.defaultInstance().constructCollectionType(List.class, Response.class));
 
         return responses;
     }
