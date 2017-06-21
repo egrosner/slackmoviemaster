ec2bdb09502cba14e2b51df40debb5b4f4a1c1df
egrosner <egrosner@gmail.com>
2017-06-20 22:05:51 -0400

    added google app engine plugin

--- src/main/java/com/erich/grosner/slackmoviemaster/web/WebController.java
+++ src/main/java/com/erich/grosner/slackmoviemaster/web/WebController.java
@@ -1,6 +1,8 @@
 package com.erich.grosner.slackmoviemaster.web;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,5 +13,13 @@ public class WebController {
     @GetMapping
     public String start() {
         return "booting up...";
+    }
+
+    //intercept from radarr / sonarr
+    @PostMapping
+    public String receive(JsonNode json) {
+
+
+        return "OK";
     }
 }
